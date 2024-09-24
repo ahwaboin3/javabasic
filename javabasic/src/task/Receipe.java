fdde8b58e5228e4169da39171893ff6c45955c37
ahwaboin3 <ahwaboin3@gmail.com>
2024-09-24 16:21:24 +0900

    4545g3v435g

---
+++ javabasic/src/task/Receipe.java
@@ -0,0 +1,19 @@
+package task;
+
+public class Receipe {
+
+	public static void main(String[] args) {
+		int four=4;
+		int g300=300;
+		int one=1;
+		int two=2;
+		double oneOfTwo=0.5;
+		String slightly="톡톡";
+		String suitable="적당량";
+		
+		System.out.println("고구마 카레덮밥 만들기");
+		System.out.println("소고기를 사용했는데요, 돼지고기로 해도 좋아요. 고형카레를 이용하는데 보이는"+ two +"개를 가장 많이 쓰고 있어요. 분말 카레로 하셔도 됩니다.");
+		System.out.println("양파가 투명해지면 고기를 넣고 계속 볶아주세요.이 때 미림(맛술)"+two+"큰술 넣고요.");
+	}
+
+}
