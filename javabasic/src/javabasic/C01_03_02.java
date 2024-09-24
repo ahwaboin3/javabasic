fb23342a4d409d9b39e26d691b9293c8e8ec1f9e
ahwaboin3 <ahwaboin3@gmail.com>
2024-09-24 12:50:17 +0900

    45453b6h456

---
+++ javabasic/src/javabasic/C01_03_02.java
@@ -0,0 +1,11 @@
+package javabasic;
+
+public class C01_03_02 {
+	
+	public static void main(String[] args) {
+		System.out.println("02");
+	}
+
+}
+//12:50까지 전까지 클래스 만들고 메인 메소드 선언하고
+//아무 문자열 콘솔에 출력 하는거 10번 하기
---
+++ javabasic/src/task/T001.java
@@ -0,0 +1,6 @@
+package task;
+
+public class T001 {
+	//메인 메소드+ 콘솔 출력 한문장
+	//T002~T010 -> 10번 하기
+}
