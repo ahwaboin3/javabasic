028b360598756ffa34ee5fe800019d1a35936008
ahwaboin3 <ahwaboin3@gmail.com>
2024-09-26 09:49:35 +0900

    3b56g36e5h

---
+++ javabasic/src/c03/C03_01_02.java
@@ -0,0 +1,25 @@
+package c03;
+
+public class C03_01_02 {
+	public static void main(String[] args) {
+		//이항 연산자
+		//피연산자가 2개인 연산자
+		//산술 연산자, 문자열 결합 연산자, 비교 연산자, 논리 연산자,대입 연산자
+		
+		//산술 연산자
+		// 연산식|설명
+		// +|덧셈 연산
+		// -|뺄셈 연산
+		// *|곱센 연산
+		// /|나눗셈 연산
+		// %|나눈 나머지를 구하는 연산
+		
+		int num1=10;
+		int result1=num1%3;
+		System.out.println(result1);
+		int result2=num1/3;
+		System.out.println(result2);
+		
+		
+	}
+}
