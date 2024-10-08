634fe8b4ec7d96167e3ab0289d32fec0b49831f7
ahwaboin3 <ahwaboin3@gmail.com>
2024-10-08 14:54:49 +0900

    3456g63

---
+++ javabasic/src/c08/herit/InterfaceA.java
@@ -0,0 +1,5 @@
+package c08.herit;
+
+public interface InterfaceA {
+
+}
---
+++ javabasic/src/c08/herit/InterfaceC.java
@@ -0,0 +1,8 @@
+package c08.herit;
+
+//인터페이스 상속
+//인터페이스도 다른 인터페이스를 상속할 수 있습니다.
+//인터페이스는 클래스와는 달리 다중 상속을 허용합니다.
+public interface InterfaceC extends InterfaceA, interfaceB{
+
+}
